9b47c44af990861798e1aebc31d940f2ac5ff420
karthik-creation2311 <karthik20021123@gmail.com>
2024-08-18 17:56:44 +0530

    feat: add values to db

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./models/product.models.js");
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello from Node API server");
+app.get("/", (req, res) => {
+  res.send("Hello from Node API server");
 });
 
-app.post('/api/create',(req,res) => {
-    
-})
+app.post("/api/create", async (req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
 
-mongoose.connect("mongodb://localhost:27017/")
-.then(() =>{
+
+
+
+//   try {
+//     const product = Product.create
+//     Product.res.status(200).json(product);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/crud")
+  .then(() => {
     console.log("connected to database!");
     app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+      console.log("Server is running on port 3000");
     });
-})
-.catch(() =>{
+  })
+  .catch(() => {
     console.log("Connection failed to database!");
-})
-
+  });
--- models/product.models.js
+++ models/product.models.js
@@ -1,29 +1,31 @@
-import mongoose, { model } from "mongoose";
-
-const ProductSchema = mongoose.Schema({
-    name:{
-        type : String,
-        require :[true,"Product name required"],
+const mongoose = require("mongoose");
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Product name required"],
     },
-    quantity :{
-        type : Number,
-        require : true,
-        default :0
+    quantity: {
+      type: Number,
+      require: true,
+      default: 0,
     },
-    price : {
-        type : Number,
-        require : true,
-        default :0
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
     },
-    image :{
-        type : String,
-        require : false,
-    }
-},
-{
-    Timestamp : true,
-});
+    image: {
+      type: String,
+      require: false,
+    },
+  },
+  {
+   Timestamp:true,
+  }
+);
 
-const Product = mongoose.model("Product",ProductSchema);
+const Product = mongoose.model("Product", ProductSchema);
 
-model.export = Product;
+module.exports = Product;
+
