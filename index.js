ee73c08e5b9424ec34e339b64e9871096eb91454
karthik-creation2311 <karthik20021123@gmail.com>
2024-08-18 18:37:31 +0530

    feat: read: display all values from database

--- index.js
+++ index.js
@@ -9,25 +9,37 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API server");
 });
 
+//add a values to database
 app.post("/api/create", async (req, res) => {
-    // console.log(req.body);
-    // res.send(req.body);
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message : error.message});
-    }
+  // console.log(req.body);
+  // res.send(req.body);
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+//read all values from database
+app.get("/api/values", async (req, res) => {
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-
-
-//   try {
-//     const product = Product.create
-//     Product.res.status(200).json(product);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
+//read values by id
+app.get("/api/value/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
